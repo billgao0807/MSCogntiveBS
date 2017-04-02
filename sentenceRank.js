70c4e82bb82023cc1232a853383a4f9b9a1eb1fc
Jeffrey Vaudrin-McLean <vaudrinm@gmail.com>
2017-04-02 02:38:43 -0700

    fixed for multi-word key phrases... still times out and doesn't run it from the actual site

--- sentenceRank.js
+++ sentenceRank.js
@@ -3,7 +3,23 @@
 // Return array of sentences ranked above average.
 function sentenceRank(textBlock, keywords) {
     //var allWords = textBlock.split(" ");
-    var allSentences = textBlock.split(".");
+    // "1、2、3".split(/(?!、)/g)
+    var sentencesAndEndings = textBlock.split(/([.!?]+)/);
+
+    var allSentences = [];
+
+    var regexI = 0;
+    for (regexI = 0; regexI < sentencesAndEndings.length - 1; regexI += 2) {
+        allSentences.push(sentencesAndEndings[regexI] + sentencesAndEndings[regexI + 1]);
+    }
+
+    // var jeffsRes = "";
+    // for (i in allSentences) {
+    //     jeffsRes += allSentences[i] + "\n";
+    // }
+    // alert("with endings:");
+    // alert(jeffsRes);
+    
     var keywordsToValues = [];
     var sentenceToKeywords = {};
     var sentenceToScore = {};
@@ -16,30 +32,51 @@ function sentenceRank(textBlock, keywords) {
         keywordsToValues[keyToLower] = 0.0;
     }
 
+    alert("about to do the craziness");
     // Count occurences of every keyword in sentence.
     // Count occurrences of every keyword in total.
     for(i in allSentences) {
         var sentence = allSentences[i];
-        var words = sentence.split(" ");
-        for(j in words) {
-            var word = words[j];
-            var wordL = word.toLowerCase();
-            // If is keyword.
-            if(wordL in keywordsToValues) {
-                // Only create a mapping if the sentence contains a keyword.
 
+        // capture the sentence in lowercase
+        var lowerSentence = sentence.toLowerCase();
+        // var words = sentence.split(" ");
+
+        // for every keyword
+        for(word in keywordsToValues) {
+            // form a regular expression to represent the "word" (can be a phrase)
+            var regExWord = word.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+            // alert("reg ex word:" + regExWord);
+            var regEx = new RegExp(word, "g");
+
+            // find the number of occurrences of the word/expression in the sentence (array is null if none found, hence the " || []")
+            var count = (lowerSentence.match(regEx) || []).length;
+
+            // If is keyword.
+            if(count > 0) {
+
+                // Only create a mapping if the sentence contains a keyword.
                 if(!(sentence in sentenceToKeywords)) {
                     sentenceToKeywords[sentence] = {};
                 }
+
                 // If keyword not already in the sentenceToKeywords array, create.
                 if(!(word in sentenceToKeywords[sentence])) {
-                    sentenceToKeywords[sentence][wordL] = 0.0;
+                    sentenceToKeywords[sentence][word] = 0.0;
                 }
-                sentenceToKeywords[sentence][wordL]+=1.0;
-                keywordsToValues[wordL]+=1.0;
+
+                sentenceToKeywords[sentence][word]+=count;
+                keywordsToValues[word]+=count;
             }
         }
     }
+
+    // var jeffsRes = "";
+    // for (word in keywordsToValues) {
+    //     jeffsRes += word + " " + keywordsToValues[word] + "\n";
+    // }
+    // alert("keywords to values after all the madness:");
+    // alert(jeffsRes);
 
     // Calculate heuristic value of keywords.
     var maxCount = 0.0;
@@ -68,7 +105,7 @@ function sentenceRank(textBlock, keywords) {
         }
         totalScore += sentenceToScore[sentence];
     }
-    alert("maxscore:"+maxScore);
+    // alert("maxscore:"+maxScore);
 
     var avgScore = totalScore / allSentences.length;
 
