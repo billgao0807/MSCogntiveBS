159ba0bdb7562d521a60374645f8c738476ba255
Rena <renachen@usc.edu>
2017-04-01 22:10:32 -0700

    integrating

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var outputText = document.getElementById("outputText");
+//var content = outputText.innerHtml;
+content = "I have a dream. Nothing is a dream. I have potatoes, and I love bananas. Fight on!";
+function summarizeText() {
+    alert("button pressed");
+    analyzeText(content);
+}
